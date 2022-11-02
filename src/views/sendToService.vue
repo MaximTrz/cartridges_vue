1f4cdcf38edbdaab692f902fe785de489bb68ce4
Maxim <maxim.trz@gmail.com>
2022-11-02 11:31:08 +0500

    sendToService view

--- src/views/sendToService.vue
+++ src/views/sendToService.vue
@@ -2,15 +2,22 @@
   <h2>Отправить в сервисный центр</h2>
   <div class="addForm">
     <div class="input-group">
-      <select class="form-select">
-        <option :value="null" selected>Выберите картридж</option>
-        <template v-if="cartidgies.length > 0">
-          <option v-for="item in cartidgies" :value="item.id" :key="item.id">
+      <select class="form-select" v-model="selectedHeadItem">
+        <option :value="null" selected>
+          <slot name="itemName"> Выберите элемент </slot>
+        </option>
+        <template v-if="items.length > 0">
+          <option v-for="item in items" :value="item.id" :key="item.id">
             {{ item.name }}
           </option>
         </template>
       </select>
-      <button class="btn btn-outline-secondary" type="button">
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        :disabled="!activeAddButton"
+        @click="addItem"
+      >
         Добавить в список
       </button>
     </div>
@@ -24,17 +31,27 @@
         <th scope="col"></th>
       </tr>
     </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td><a class="table_link" href="#">5</a></td>
-        <td><a class="table_link" href="#">Cactus 505 </a></td>
-        <td><button class="btn btn-danger">X</button></td>
+    <tbody v-if="tableItems.length > 0">
+      <tr v-for="(item, index) in tableItems" :key="item.id">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>
+          <a class="table_link" href="#">{{ item.id }}</a>
+        </td>
+        <td>
+          <a class="table_link" href="#"> {{ item.name }} </a>
+        </td>
+        <td>
+          <button class="btn btn-danger" @click="deleteItem(item)">X</button>
+        </td>
       </tr>
     </tbody>
   </table>
   <div class="save">
-    <button class="btn btn-outline-success btn-lg" type="button">
+    <button
+      class="btn btn-outline-success btn-lg"
+      type="button"
+      :disabled="saveActive"
+    >
       Сохранить
     </button>
   </div>
@@ -45,12 +62,47 @@ export default {
   name: "sendToService",
   data: function () {
     return {
-      cartidgies: [
+      items: [
         { id: 1, name: "Cactus 505" },
         { id: 2, name: "Cactus 48X" },
         { id: 3, name: "Cactus 055" },
       ],
+      selectedHeadItem: null,
+      slelectedTableItem: null,
+      tableItems: [],
     };
+  },
+  methods: {
+    addItem() {
+      const item = this.items.find((item) => item.id == this.selectedHeadItem);
+      this.tableItems.push(item);
+    },
+    deleteItem(item) {
+      this.tableItems = this.tableItems.filter((elem) => elem != item);
+    },
+  },
+  computed: {
+    activeAddButton: function () {
+      return this.selectedHeadItem != null && !this.checkDoubleItem;
+    },
+    checkDoubleItem: function () {
+      return (
+        this.tableItems.findIndex((item) => item.id == this.selectedHeadItem) >
+        -1
+      );
+    },
+    saveActive: function () {
+      return !this.tableItems.length > 0;
+    },
   },
 };
 </script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+.addForm {
+  margin-bottom: 30px;
+}
+</style>
